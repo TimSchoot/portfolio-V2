b85a09cf217703c743554f0dd692bdd25b64d3ad
TimSchoot <t.vanderschoot@student.alfa-college.nl>
2024-07-02 16:28:46 +0200

    nieuwe webpages

--- header/index.php
+++ header/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>portfolio</title>
+    <title>Portfolio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="../style.css" rel="stylesheet">
 </head>
@@ -18,27 +18,32 @@
                 </button>
                 <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                     <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">navigatie</h5>
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Navigatie</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body">
                         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                             <div class="list-group">
-                                <a href="../over/index.php" class="list-group-item list-group-item-action">over mij</a>
-                                <a href="../socials/index.php" class="list-group-item list-group-item-action">contact</a>
+                                <a href="../over/index.php" class="list-group-item list-group-item-action">Over mij</a>
+                                <a href="../socials/index.php" class="list-group-item list-group-item-action">Contact</a>
+                                <a href="http://github.com/TimSchoot" class="list-group-item list-group-item-action">Github</a>
                             </div><br>
                             <li class="nav-item  dropdown">
                                 <a class="nav-link list-group dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     <B>Projecten</B>
                                 </a>
                                 <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" target="_blank" href="http://rekenen.timschoot.nl/">reken website</a></li>
+                                    <li><a class="dropdown-item" target="_blank" href="http://rekenen.timschoot.nl/">Reken website</a></li>
                                     <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" target="_blank" href="http://irritanteonepager.timschoot.nl">irritante onepager</a></li>
+                                    <li><a class="dropdown-item" target="_blank" href="http://onepager.timschoot.nl">Onepager</a></li>
                                     <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" target="_blank" href="http://webgame.timschoot.nl">Webgame</a></li>
+                                    <li><a class="dropdown-item" target="_blank" href="http://irritanteonepager.timschoot.nl">Irritante onepager</a></li>
                                     <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" target="_blank" href="https://github.com">github</a></li>
+                                    <li><a class="dropdown-item" target="_blank" href="http://webgame.timschoot.nl">Webgame(kleine bug)</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" target="_blank" href="http://rekenrace.timschoot.nl">Reken race</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><a class="dropdown-item" target="_blank" href="http://webshop.timschoot.nl">webshop(niet af)</a></li>
                                 </ul>
                             </li>
                         </ul>
